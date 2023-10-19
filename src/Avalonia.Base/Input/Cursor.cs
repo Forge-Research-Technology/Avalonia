20ca4550921dad2a825a1ecbfb57b759764d85d4
Jan Kristian Bjerke <jan.bjerke@gmail.com>
2023-10-19 18:32:01 +0200

    Implement cursor scaling

--- src/Avalonia.Base/Input/Cursor.cs
+++ src/Avalonia.Base/Input/Cursor.cs
@@ -39,10 +39,26 @@ namespace Avalonia.Input
         // SizeNorthEastSouthWest,
     }
 
+    public class ScaledCursor
+    {
+        public ScaledCursor(Bitmap cursor, PixelPoint hotSpot, double scale)
+        {
+            Cursor = cursor;
+            HotSpot = hotSpot;
+            Scale = scale;
+        }
+
+        public Bitmap Cursor { get; }
+        public PixelPoint HotSpot { get; }
+        public double Scale { get; }
+    }
+
     public class Cursor : IDisposable
     {
         public static readonly Cursor Default = new Cursor(StandardCursorType.Arrow);
         private string _name;
+        private ScaledCursor[]? _scaledImages;
+        private int _loadedScaleIndex = 0;
 
         private Cursor(ICursorImpl platformImpl, string name)
         {
@@ -60,9 +76,60 @@ namespace Avalonia.Input
         {
         }
 
-        internal ICursorImpl PlatformImpl { get; }
+        public Cursor(params ScaledCursor[] scaledImages)
+            : this(GetCursorFactory().CreateCursor(scaledImages[0].Cursor.PlatformImpl.Item, scaledImages[0].HotSpot), "BitmapCursor")
+        {
+            _scaledImages = scaledImages;
+        }
+
+        internal ICursorImpl PlatformImpl { get; private set; }
 
         public void Dispose() => PlatformImpl.Dispose();
+
+        public void Scale(double scale)
+        {
+            if (_scaledImages is null)
+                return;
+
+            var bestIndex = FindClosestScaleIndex(scale);
+
+            if (bestIndex == _loadedScaleIndex)
+                return;
+
+            var bestImage = _scaledImages[bestIndex];
+
+            PlatformImpl.Dispose();
+
+            PlatformImpl = GetCursorFactory().CreateCursor(bestImage.Cursor.PlatformImpl.Item, bestImage.HotSpot);
+
+            _loadedScaleIndex = bestIndex;
+        }
+
+        private int FindClosestScaleIndex(double scale)
+        {
+            if(_scaledImages is null)
+                return -1;
+
+            var bestIndex = -1;
+            var bestScale = 0.0;
+
+            for (int i = 0; i < _scaledImages.Length; i++)
+            {
+                var image = _scaledImages[i];
+                if (bestIndex == -1)
+                {
+                    bestIndex = i;
+                    bestScale = image.Scale;
+                }
+                else if(Math.Abs(image.Scale - scale) < Math.Abs(bestScale - scale))
+                {
+                    bestIndex = i;
+                    bestScale = image.Scale;
+                }
+            }
+
+            return bestIndex;
+        }
 
         public static Cursor Parse(string s)
         {
