c77a4ef18cc7cf7af5d9be6df833e3b965f89b6c
Jan Kristian Bjerke <jan.bjerke@gmail.com>
2023-11-07 08:40:14 +0100

    Update ISpecialOverlayWindow.cs

--- src/Avalonia.Controls/Platform/ISpecialOverlayWindow.cs
+++ src/Avalonia.Controls/Platform/ISpecialOverlayWindow.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Avalonia.Input;
+using Avalonia.Input.Raw;
 
 namespace Avalonia.Platform
 {
     public interface ISpecialOverlayWindow
     {
         public Action<string> FirstResponderChanged { get; set; }
+
         public Action<Point> OnSlideMouseActivate { get; set; }
 
         public Func<Point, bool> ShouldPassThrough { get; set; }
+
+        public Func<RawKeyEventType, Key, RawInputModifiers, bool> MonitorKeyEvent { get; set; }
     }
 }
