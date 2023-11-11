1ac3837f5faf49d10680ba8af4c21ee348ce65a7
jankrib <jan.bjerke@gmail.com>
2023-11-11 10:14:17 +0100

    Update override-mac.sh

--- override-mac.sh
+++ override-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ditto build/Products/Release/libAvalonia.Native.OSX.dylib /Library/Application\ Support/Microsoft/Grunt/Grunt.plugin/Contents/MacOS/libAvaloniaNative.dylib
-
-for f in src/Avalonia.Native/bin/Debug/net6.0/*; do
+for f in src/Avalonia.Desktop/bin/Release/net6.0/*; do
   cp -fv "$f" "/Library/Application Support/Microsoft/Grunt/Grunt.plugin/Contents/MacOS/"
 done
+
+ditto build/Products/Release/libAvalonia.Native.OSX.dylib /Library/Application\ Support/Microsoft/Grunt/Grunt.plugin/Contents/MacOS/libAvaloniaNative.dylib
